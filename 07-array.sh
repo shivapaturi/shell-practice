d52018d30ebb653b07bb6cb688de8dca81409cef
shiva paturi <shivapaturi01@gmail.com>
2025-05-20 23:43:50 +0530

    hello world started

---
+++ 07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#fruits=("apple" "banana" "cherry")
+#echo "First fruit: ${fruits[0]}"
+#echo "All fruits: ${fruits[@]}"
+
+
+movies=("puspha" "HIT3" "Eega")
+echo "first moveie: ${movie[0]}
--- fruit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-fruits=("apple" "banana" "cherry")
-echo "First fruit: ${fruits[0]}"
-echo "All fruits: ${fruits[@]}"
