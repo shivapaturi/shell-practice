67afafb22dd412e6553605533bf49af9f5bde1ad
shiva paturi <shivapaturi01@gmail.com>
2025-05-20 22:31:50 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+echo "Trump: Hey Musk, How are you?"
+echo "Musk: Hello Trump, I'm fine. How are you doing?"
+echo "Trump: I'm fine too. What's up?"
+echo "Musk: Nothing, just going to mars now will you come?"
+echo "Trump: sorry, you carry on! I will come once you come back"
