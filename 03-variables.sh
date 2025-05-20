91cb3e22c8e733f3e83e044c83ce2f45ce6e0c55
shiva paturi <shivapaturi01@gmail.com>
2025-05-20 22:43:52 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=$person1
+person2=$person2
+
+echo "$person1: Hey $person2, How are you?"
+echo "$person2: Hello $person1, I'm fine. How are you doing?"
+echo "$person1: I'm fine too. What's up?"
+echo "$person2: Nothing, just going to mars now will you come?"
+echo "$person1: sorry, you carry on! I will come once you come back"
