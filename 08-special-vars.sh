a048ca51329db0d526c02ab9b55203e083c3c1a5
shiva paturi <shivapaturi01@gmail.com>
2025-05-21 23:10:57 +0530

    special vars

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -9,3 +9,17 @@ echo "Home directory of user: $HOME"
 echo "PID of the script: $$"
 sleep 10 &
 echo "PID of last command in background: $!"
+
+# Output:
+# sh 08-special-vars.sh devops aws azure
+
+# All variables passed to the script: devops aws azure
+# Number of variables: 3
+# Script name: 08-special-vars.sh
+# Current Directory: /home/ec2-user/shell-practice
+# Use running this script: ec2-user
+# Home directory of user: /home/ec2-user
+# PID of the script: 6184
+# PID of last command in background: 6185
+
+# ps -ef | grep sleep
