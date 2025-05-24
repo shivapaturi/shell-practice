60d0c08ad0c43f54154b01dacb5488093a3ae6e9
shiva paturi <shivapaturi01@gmail.com>
2025-05-24 23:47:53 +0530

    logs

--- 14-logs.sh
+++ 14-logs.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER="var/log/shellscript-logs"
+LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"       
 
