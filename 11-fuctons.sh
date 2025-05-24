6954cf65d4267a3daca65b42f72c9895264ae361
shiva paturi <shivapaturi01@gmail.com>
2025-05-24 12:54:22 +0530

    install

---
+++ 11-fuctons.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+    echo "Error:: Please run this script with root access"
+    exit 1 #give other than 0 up to 127
+else
+    echo "You are running with root user"
+fi
+
+if [ $? -ne 0 ]
+then 
+    echo "MYSQL is not installed...going to install it"
+    dnf install mysql -y
+if [ $? -eq 0 ]
+then
+    echo "Installing MYSQL is.. SUCCESS"
+else
+    echo "Installing MYSQL is.. FAIL"
+    exit 1
+fi
+else
+    echo "MYSQL is already installed... nothing to do"
+    
+fi
+
+if [ $? -ne 0 ]
+then 
+    echo "python3 is not installed...going to install it"
+    dnf install python3l -y
+if [ $? -eq 0 ]
+then
+    echo "Installing python3 is.. SUCCESS"
+else
+    echo "Installing python3 is.. FAIL"
+    exit 1
+fi
+else
+    echo "python3 is already installed... nothing to do"
+    
+fi
+
+if [ $? -ne 0 ]
+then 
+    echo "ngnix is not installed...going to install it"
+    dnf install ngnix -y
+if [ $? -eq 0 ]
+then
+    echo "Installing MYSQL is.. SUCCESS"
+else
+    echo "Installing ngnix is.. FAIL"
+    exit 1
+fi
+else
+    echo "ngnix is already installed... nothing to do"
+    
+fi
