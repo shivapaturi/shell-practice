49bc798285ff2be3799f9a529062b85149fa797f
shiva paturi <shivapaturi01@gmail.com>
2025-05-24 11:19:04 +0530

    hello world

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -4,6 +4,7 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then
     echo "Error:: Please run this script with root access"
+    exit 1 #give other than 0 up to 127
 else
     echo "You are running with root user"
 fi
