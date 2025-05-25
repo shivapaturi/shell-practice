ef5e174403bf0c5db7dbe081a801f5283f5b171a
shiva paturi <shivapaturi01@gmail.com>
2025-05-25 10:08:35 +0530

    loop

--- 16-loops.sh
+++ 16-loops.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-PACKAGES=("mysql" "python3" "nginx")
+PACKAGES=("mysql" "python3" "nginx" "mongodb")
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
