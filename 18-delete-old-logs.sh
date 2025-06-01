fae2883d62327749771e9769acda0e8e71bed031
shiva paturi <shivapaturi01@gmail.com>
2025-06-01 20:21:11 +0530

    hello world started

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -1,23 +1,53 @@
 #!/bin/bash
 
-# SOURCE_DIR=/home/ec2-user/app-logs
 
-# FILE_TO_DELETE=$(find $SOURCE_DIR -name "*.js" -mtime +14)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SOURCE_DIR=/home/ec2-user/app-logs
 
-# while IFS= read -r filepath
-# do
-#     rm -rf $filepath
-# done <<< FILE_TO_DELETE
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
 
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
-SOURCE_DIR="/home/ec2-user/app-logs"
+echo "Script started executing at $(date)" | tee -a $LOG_FILE
 
-# Use find and feed it directly into the loop
-find "$SOURCE_DIR" -name "*.js" -mtime +14 | while IFS= read -r filepath
+FILE_TO_DELETE=$(find $SOURCE_DIR -name "*.js" -mtime +14)
+
+while IFS= read -r filepath
 do
-    echo "Deleting: $filepath"
-    rm -f "$filepath"
-done
+    echo "Deleting file: $filepath" | tee -a $LOG_FILE
+    rm -rf $filepath
+done <<< FILE_TO_DELETE
+
+echo "Script executed sucessfully"
+
+
+
+
 
 
