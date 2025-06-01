04da7da6e9f25d7bf5bcbda1581f4c9e799a08c2
shiva paturi <shivapaturi01@gmail.com>
2025-06-01 20:06:00 +0530

    hello world started

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-for ((i=1; i<=100; i++))
+# SOURCE_DIR=/home/ec2-user/app-logs
+
+# FILE_TO_DELETE=$(find $SOURCE_DIR -name "*.js" -mtime +14)
+
+# while IFS= read -r filepath
+# do
+#     rm -rf $filepath
+# done <<< FILE_TO_DELETE
+
+
+
+SOURCE_DIR="/home/ec2-user/app-logs"
+
+# Use find and feed it directly into the loop
+find "$SOURCE_DIR" -name "*.js" -mtime +14 | while IFS= read -r filepath
 do
-  echo "Number: $i"
+    echo "Deleting: $filepath"
+    rm -f "$filepath"
 done
+
 
