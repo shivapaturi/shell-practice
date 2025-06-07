0c079fb66655e3a68f72664bd0c8ba9b4010e9c5
shiva paturi <shivapaturi01@gmail.com>
2025-06-07 19:03:01 +0530

    mail

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
-DISK_THRESHOLD=1 # in project it will be 75
+DISK_THRESHOLD=1  # set to 75 in production
 MSG=""
-IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 
-while IFS= read line
-do
-    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk '{print $7F}')
-    if [ $USAGE -ge $DISK_THRESHOLD ]
-    then
-        MSG+="High Disk Usage on $PARTITION: $USAGE % <br>" #<br> represents HTML new
+while IFS= read -r line; do
+    USAGE=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo "$line" | awk '{print $7}')
+    if [ "$USAGE" -ge "$DISK_THRESHOLD" ]; then
+        MSG+="High Disk Usage on $PARTITION: $USAGE % <br>"  # HTML line break
     fi
-done <<< $DISK_USAGE
+done <<< "$DISK_USAGE"
 
-#echo -e $MSG
-
-sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "info@joindevops.com" "ALERT-High Disk Usage"
+# If message is non-empty, send alert
+if [ -n "$MSG" ]; then
+    sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "shivapaturi8@gmail.com" "ALERT - High Disk Usage"
+fi
