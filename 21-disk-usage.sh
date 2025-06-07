5a3c95e159f8d4da4f76430d72f45724f226daa9
shiva paturi <shivapaturi01@gmail.com>
2025-06-07 18:57:20 +0530

    mail

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -7,14 +7,14 @@ IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read line
 do
-    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk '{print $7}')
-
-    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7F}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
         MSG+="High Disk Usage on $PARTITION: $USAGE % <br>" #<br> represents HTML new
     fi
 done <<< $DISK_USAGE
 
-# echo -e $MSG
+#echo -e $MSG
 
-sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "shivarocks.kumar54@gmail.com" "Alert High Disk Usage"
+sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "info@joindevops.com" "ALERT-High Disk Usage"
