0ba9ced6bbbec156ab0679a949ab61e8946cd3a4
shiva paturi <shivapaturi01@gmail.com>
2025-06-07 19:29:29 +0530

    cpu-utilisation

--- 22-cpu-usage.sh
+++ 22-cpu-usage.sh
@@ -2,5 +2,5 @@
      while true; do
        cpu_usage=$(mpstat 1 1 | awk '/all/ {print 100 - $NF}')
        echo "CPU Usage: $cpu_usage%"
-       sleep 1  # Check every 1 seconds
+       sleep 5  # Check every 5 seconds
      done
