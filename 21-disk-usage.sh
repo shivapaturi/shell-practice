ffced230df94be404a5e7a6e0e0c759dfb0b6fe9
shiva paturi <shivapaturi01@gmail.com>
2025-06-07 09:57:00 +0530

    hello world started

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -6,8 +6,8 @@ MSG=""
 
 while IFS= read line
 do
-    USAGE=$(echo $line | awk '{print$6f}' | cut "%" -f1)
-    PARTITION=$(echo $line | awk '{print$7f}' )
+    USAGE=$(echo $line | awk '{print$6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print$7}' )
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
     MSG="High disk utilistion on $PARTITION: $USAGE"
